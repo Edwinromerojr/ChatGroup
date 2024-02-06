6566c7d94ac63a38a0d946ffd9426eb37e204247
Edwin Romero <edwinrmrjr@gmail.com>
2024-02-07 07:39:44 +0800

    activity feed filter

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -61,14 +61,16 @@ def registerPage(request):
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
+
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
         Q(description__icontains=q)
         )
+
     topics = Topic.objects.all()
     room_count = rooms.count()
-    room_messages = Message.objects.all()
+    room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
     context = {'rooms': rooms, 'topics':topics,
                'room_count': room_count, 'room_messages':room_messages}
